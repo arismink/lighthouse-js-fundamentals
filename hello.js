bb01ba7942aef20dcfa77a3658b762a373804843
Krismina <krismina.la@gmail.com>
2022-01-19 01:22:49 +0000

    using function expressions to create a string

--- hello.js
+++ hello.js
@@ -5,3 +5,15 @@ const sayHello = function (name) {
 sayHello("Caliban");
 sayHello("Miranda");
 sayHello("Ferdinand");
+
+
+/* ------------------------------------------------------------------------------------- */
+
+
+/* return vs console.log */
+
+const returnSayHello = function(name) {
+    return "Hello, " + name;
+}
+const greeting = returnSayHello("John");
+console.log(greeting);
