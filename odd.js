0e740e433786abd0fff01b388e7e6ac0bcdac998
Krismina <krismina.la@gmail.com>
2022-01-19 01:43:24 +0000

    function expression to determine if value is odd

---
+++ odd.js
@@ -0,0 +1,7 @@
+// take a number as parameter and return true if odd, false otherwise
+
+var isOdd = function (x) {
+    return x % 2 !== 0;
+}
+console.log("3 is odd: " + isOdd(3));
+console.log("8 is odd: " + isOdd(8));
